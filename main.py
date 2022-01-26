c2eed1fc4de31968cd85b59c8f88cd8c655aee6f
jsantore <jsantore@bridgew.edu>
2022-01-25 19:51:17 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+
+import requests
+
+def main():
+    loc = "http://universities.hipolabs.com/search?name=Young"
+    results = requests.get(loc)
+    print(results)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
